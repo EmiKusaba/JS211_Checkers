5c67c298f60b6750bc387ea6034e9b58c82e1595
EmiKusaba <eeemi615@gmail.com>
2021-12-20 15:25:41 -0600

    added checker and createChecker

--- 2ndTry.js
+++ 2ndTry.js
@@ -6,6 +6,18 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+//checker
+class Checker {
+  constructor(isWhite) {
+    this.isWhite = isWhite
+    if (isWhite) {
+      this.symbol = String.fromCharCode(0x125CB)
+    } else {
+      this.symbol = String.fromCharCode(0x125CF)
+    }
+  }
+}
+
 
 //board
 class Board {
@@ -21,15 +33,39 @@ class Board {
     //     [null, null, null, null, null, null, null, null],
     //     [null, null, null, null, null, null, null, null]
     // ]
-      for (let i = 0; i < 8; i++) {
-        let row = []
-        for (let j = 0; j < 8; j++) {
-          row.push[null]
-        }
-        this.grid.push(row)
+    for (let i = 0; i < 8; i++) {
+      let row = []
+      for (let j = 0; j < 8; j++) {
+        row.push[null]
       }
+      this.grid.push(row)
     }
   }
-//checker
 
+  createCheckers() {
+    const whitePositions = [[0, 1], [0, 3], [0, 5], [0, 7],
+    [1, 0], [1, 2], [1, 4], [1, 6],
+    [2, 1], [2, 3], [2, 5], [2, 7]];
+
+    const blackPositions = [[5, 0], [5, 2], [5, 4], [5, 6],
+    [6, 1], [6, 3], [6, 5], [6, 7],
+    [7, 0], [7, 2], [7, 4], [7, 6]];
+
+
+    for (let i = 0; i < whitePositions.length; i++) {
+      let white = new Checker(true)
+      let pos = whitePositions[i]
+      let row = pos[0]
+      let col = pos[1]
+      this.grid[row][col] = white
+    }
+    for (let i = 0; i < blackPositions.length; i++) {
+      let black = new Checker(false)
+      let pos = blackPositions[i]
+      let row = pos[0]
+      let col = pos[1]
+      this.grid[row][col] = black
+    }
+  }
+}
 //Game
